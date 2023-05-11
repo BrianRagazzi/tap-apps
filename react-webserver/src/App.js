febe4379e004ab3a5482be14462a4baeaca9879f
Brian Ragazzi <brianpragazzi@gmail.com>
2023-05-11 13:53:42 -0500

    use tanzu logo

--- react-webserver/src/App.js
+++ react-webserver/src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return <div className="App">
-      <img src="https://paketo.io/images/paketo-logo-full-color.png" alt="Paketo Buildpacks logo"></img>
-      Powered by Paketo Buildpacks
+      <img src="./vmware-tanzu-icon.png" alt="VMware Tanzu logo" width=1024 height=284></img>
+      Powered by Paketo Buildpacks in Tanzu Application Platform
   </div>;
 }
 
