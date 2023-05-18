6181429028ff6314862493ce2f4930b2421a08f7
Brian Ragazzi <brianpragazzi@gmail.com>
2023-05-18 12:22:35 -0500

    Update App.js

--- react-webserver/src/App.js
+++ react-webserver/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return <div className="App">
-      <img src="./vmware-tanzu-icon.png" alt="VMware Tanzu logo" width="1024" height="284"></img>
+      <img src="./vmware-tanzu-icon.png" alt="VMware Tanzu logo" width="1024"></img>
       Powered by Paketo Buildpacks in Tanzu Application Platform
   </div>;
 }
