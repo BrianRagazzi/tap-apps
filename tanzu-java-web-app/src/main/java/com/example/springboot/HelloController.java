15427a504c62f485a18b2db6664062698580ec6f
Brian Ragazzi <brianpragazzi@gmail.com>
2023-06-01 17:01:14 -0500

    Update HelloController.java

--- tanzu-java-web-app/src/main/java/com/example/springboot/HelloController.java
+++ tanzu-java-web-app/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from TAP, Iterate Branch!";
+		return "Greetings from TAP, Iterate here!";
 	}
 
 }
